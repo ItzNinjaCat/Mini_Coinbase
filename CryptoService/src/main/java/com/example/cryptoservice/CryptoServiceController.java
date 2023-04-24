9105282d969f45685cf625c13c5b825cd5beeecf
yavor0 <85577690+yavor0@users.noreply.github.com>
2023-04-24 23:43:57 +0300

    improvements

--- CryptoService/src/main/java/com/example/cryptoservice/CryptoServiceController.java
+++ CryptoService/src/main/java/com/example/cryptoservice/CryptoServiceController.java
@@ -2,16 +2,17 @@ package com.example.cryptoservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/crypto")
 public class CryptoServiceController
 {
+
+    @Autowired
+    private CryptoBalanceRepository cryptoBalanceRepository;
 
     @Autowired
     private CryptoService cryptoService;
@@ -30,4 +31,38 @@ public class CryptoServiceController
             return ResponseEntity.ok(price);
         }
     }
+
+//    @PostMapping("/exchange")
+//    public ResponseEntity<BigDecimal> getPrice(@RequestParam long userId,
+//                                               @RequestParam int amount,
+//                                               @RequestParam String crypto,
+//                                               @RequestParam String cryptoReceive)
+//    {
+//        CryptoBalance user = cryptoBalanceRepository.findByUserIdAndCrypto(userId, crypto).orElseThrow();
+//        BigDecimal userCryptoAmount = user.getBalance();
+//        if (userCryptoAmount.compareTo(BigDecimal.valueOf(amount)) < 0) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//
+//        double rate = getExchangeRate(crypto, cryptoReceive);
+//
+//        double receivedAmount = amount * rate;
+//
+//        user.setBalance(userCryptoAmount.subtract(BigDecimal.valueOf(amount)));
+//
+//        cryptoBalanceRepository.save(user);
+//        return ResponseEntity.ok("Exchanged " + amount + " " + crypto + " for " + receivedAmount + " " + cryptoReceive).build();
+//
+//    }
+
+//    private double getExchangeRate(String cryptoFrom, String cryptoTo) {
+//        if (cryptoFrom == cryptoTo) {
+//            return 1.0;
+//        }
+//
+//        double cryptoFromValue = cryptoService.getPrice(cryptoFrom, "usd").doubleValue();
+//        double cryptoToValue = cryptoService.getPrice(cryptoTo, "usd").doubleValue();
+//
+//        return cryptoFromValue / cryptoToValue;
+//    }
 }
