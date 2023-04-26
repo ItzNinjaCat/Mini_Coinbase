db271f6e805803a066f065d00260bfde5872e08f
ItzNinjaCat <47333381+ItzNinjaCat@users.noreply.github.com>
2023-04-26 05:11:43 +0300

    auth changes

--- TransactionService/src/main/java/com/transactionservice/TransactionController.java
+++ TransactionService/src/main/java/com/transactionservice/TransactionController.java
@@ -1,10 +1,11 @@
 package com.transactionservice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/transaction")
@@ -13,14 +14,26 @@ public class TransactionController
     @Autowired
     private TransactionService transactionService;
 
-
+    @Autowired
+    private RestTemplate restTemplate;
     @PostMapping("/buy")
-    public ResponseEntity<?> buyCrypto(@RequestBody TransactionDto transactionDto)
+    public ResponseEntity<?> buyCrypto(@CookieValue(value = "jwt", defaultValue = "no_jwt") String jwt_token, @RequestBody TransactionDto transactionDto)
     {
-        // Check if the user is authenticated
-//        System.out.println(transactionDto);
         try
         {
+            if(jwt_token.equals("no_jwt")){
+                return ResponseEntity.badRequest().body("Not logged in");
+            }
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + jwt_token);
+            HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+            ResponseEntity<String> responseEntity = restTemplate.exchange("http://auth-service:8090/api/auth/getUser", org.springframework.http.HttpMethod.GET, requestEntity, String.class);
+            if(responseEntity.getStatusCode().isError()){
+                return ResponseEntity.badRequest().body("Invalid token");
+            }
+            String userId = responseEntity.getBody();
+            assert userId != null;
+            transactionDto.setUserId(Long.valueOf(userId));
             transactionService.buyCrypto(transactionDto);
             return ResponseEntity.ok().build();
         } catch (RuntimeException e)
@@ -30,10 +43,23 @@ public class TransactionController
     }
 
     @PostMapping("/sell")
-    public ResponseEntity<?> sellCrypto(@RequestBody TransactionDto transactionDto)
+    public ResponseEntity<?> sellCrypto(@CookieValue(value = "jwt", defaultValue = "no_jwt") String jwt_token, @RequestBody TransactionDto transactionDto)
     {
         try
         {
+            if(jwt_token.equals("no_jwt")){
+                return ResponseEntity.badRequest().body("Not logged in");
+            }
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + jwt_token);
+            HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+            ResponseEntity<String> responseEntity = restTemplate.exchange("http://auth-service:8090/api/auth/getUser", org.springframework.http.HttpMethod.GET, requestEntity, String.class);
+            if(responseEntity.getStatusCode().isError()){
+                return ResponseEntity.badRequest().body("Invalid token");
+            }
+            String userId = responseEntity.getBody();
+            assert userId != null;
+            transactionDto.setUserId(Long.valueOf(userId));
             transactionService.sellCrypto(transactionDto);
             return ResponseEntity.ok().build();
         } catch (RuntimeException e)
